9ef18490aab6730c17970ba65a4bf7a97544ae61
Joseph-Melberg <dasbasdo@gmail.com>
2022-03-16 17:09:02 +0000

    Initial commit

---
+++ plane-collector/target/debug/deps/plane_collector-1db2c92c8aa9d8c1.d
@@ -0,0 +1,5 @@
+/home/pi/Documents/Git/Plane-Collector/plane-collector/target/debug/deps/plane_collector-1db2c92c8aa9d8c1: src/main.rs
+
+/home/pi/Documents/Git/Plane-Collector/plane-collector/target/debug/deps/plane_collector-1db2c92c8aa9d8c1.d: src/main.rs
+
+src/main.rs:
---
+++ plane-collector/target/debug/plane-collector.d
@@ -0,0 +1,1 @@
+/home/pi/Documents/Git/Plane-Collector/plane-collector/target/debug/plane-collector: /home/pi/Documents/Git/Plane-Collector/plane-collector/src/main.rs
